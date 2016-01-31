ee613046f1add6367cb25aaca0a603810ade5adb
Joshua Guerra <joshua@allianceconsults.net>
2016-01-31 00:52:13 -0500

    init netbeans project for group project 1

---
+++ Group Projects/GroupProject1JGuerra/src/groupproject1jguerra/GroupProject1JGuerra.java
@@ -0,0 +1,16 @@
+/*  Phone Number Converter
+    Project Name: Group Project 1 JGuerra
+    Author: Josh Guerra
+    Created: 1.29.16
+    Modified: 1.29.16
+ */
+
+package groupproject1jguerra;
+
+public class GroupProject1JGuerra {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
